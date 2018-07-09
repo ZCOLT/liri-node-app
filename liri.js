0ec1ea12ad441c61dcfd84528b21aa45bc067c7a
ZCOLT <colton.zac@gmail.com>
2018-07-09 12:44:28 -0600

    spotify track search working

---
+++ liri.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+var keys = require("./key")
+
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
+
+// Enter Track Name: 
+spotify.search({ type: 'track', query: process.argv[2] } , function (err, data) {
+    if (err) {
+    return console.log('Error ' + err);
+}
+// console.log(data.tracks.items[0])
+console.log("Artist: " + data.tracks.items[0].artists[0].name);
+console.log("Album: " + data.tracks.items[0].album.name)
+console.log("Release Date: " + data.tracks.items[0].album.release_date);
+
+})
